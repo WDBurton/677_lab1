92e363983c446fa4d640382ed71c2e4f33418e60
William Burton <wdburton@umass.edu>
2021-03-10 19:43:21 -0500

    Actually save file before commit

--- bazaar.h
+++ bazaar.h
@@ -3,7 +3,24 @@
 #define BAZAAR_H
 
 
-
+// The functions in bazaar
 void testMultiCompile();
+
+
+
+// Constants for useage everywhere
+
+// The constants that determine what type of peer it is
+#define BUY 0
+#define SELL 1
+
+// The constants that determine what type of goods are avaiable for purchase or selling, as well as
+// how many of them there are
+#define FISH 0
+#define BOAR 1
+#define DUCK 2
+
+#define NUM_PRODUCTS 3
+
 
 #endif
