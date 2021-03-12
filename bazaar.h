e1a1642e7d83b802290a5ee107410321d6ba4cee
William Burton <wdburton@umass.edu>
2021-03-11 21:44:27 -0500

    Fixed struct-union-struct to make it actually work

--- bazaar.h
+++ bazaar.h
@@ -4,6 +4,7 @@
 
 // The 'peer' structure, used for constructing the p2p network.
 struct peer{
+    int ID;             // The ID of the peer in question
     int type;           // The type of peer, whether buyer, seller, or both
     int port;           // The port of the peer, for binding
     int neighborPort;   // The port of the neighbor, will be replaced when having more than 1 neighbor
@@ -16,7 +17,7 @@ struct peer{
 
 #define MAX_HOPS = 10
 
-// The 'message' structure, used for messages between peers in the bazaar.
+// The 'message' structure, used for messages between peers in the bazaar.  A basic struct/union/struct.
 struct bazaarMessage{
     int type;                       // The type of message; this will be determined using preprocessor constants.
     union message{                      // A union of possible messages.
@@ -25,20 +26,33 @@ struct bazaarMessage{
             int goodType;                       // The type of good being sought out.
             int hopNum;                         // The number of hops allowed.
             int prevHops[10];                   // Past hop IDs; Need to edit when figure out better method.
-        };
+        } sellerSeek;
         struct buyerFound{                  // A return to the flood, letting a seller know the buyer has been found
             int buyerID;                        // The ID of the buyer
             int sellerID;                       // The ID of the seller
             int hopNum;                         // Used with prevHops to get back to buyer
             int prevHops[10];                   // The previous hop IDs;  Need to edit when figure out/need better method           
-        };
+        } buyerFound;
         struct buy{                         // A command to buy 1 of a particular good.
             int goodType;                       // The good type -- defined with preproccesor constants.
-        };
+        } buy;
 
-    };
+    } message;
 };
 
+struct test{
+    int a;
+    union tu{
+        struct{
+            int a;
+            int b;
+        } ta;
+        struct tb{
+            char a;
+            char b;
+        };
+    } tu;
+};
 
 
 
@@ -46,8 +60,8 @@ struct bazaarMessage{
 void testMultiCompile();
 int buyer(int peerId, int portNum, int otherPort);
 int seller(int peerId, int portNum, int otherPort);
-int mOne_sellFish( struct peer peerDesc, struct sockaddr_in address );
-int mOne_buyFish( struct peer peerDesc, struct sockaddr_in address );
+int mOne_sellFish( struct peer peerDesc, struct sockaddr_in address, int peerSocket );
+int mOne_buyFish( struct peer peerDesc, struct sockaddr_in address, int peerSocket );
 int makePeer(struct peer);
 
 
@@ -61,9 +75,9 @@ int makePeer(struct peer);
 
 
 // The constants that define what type of command is being sent over the network.
-#define BUYER_SEEK 0
-#define BUYER_FOUND 1
-#define BUY 2
+#define MESSAGE_SELLER_SEEK 0
+#define MESSAGE_SELLER_FOUND 1
+#define MESSAGE_BUY 2
 
 
 
